85b7c6f48c2d0db134961f213bb9eb1c49b1f4df
Mohammad Jangda <batmoo@gmail.com>
2014-11-10 12:55:13 -0500

    Fix symlinks in init.sh

--- init.sh
+++ init.sh
@@ -2,12 +2,22 @@
 echo "Setting up dotfiles"
 
 # TODO: check actual dotfiles location
-PATH="~/dotfiles"
+DOTFILES_PATH=$(pwd)
 # TODO: check that file doesn't exist already
-ln -s $PATH/vimrc ~/.vimrc
-ln -s $PATH/vim/colors/wombat256mod.vim ~/.vim/colors/wombat256mod.vim
-ln -s $PATH/vim/colors/wombat256.vim ~/.vim/colors/wombat256.vim
-ln -s $PATH/vim/colors/wombat.vim ~/.vim/colors/wombat.vim
-ln -s $PATH/gitconfig ~/.gitconfig
+ln -s $DOTFILES_PATH/vimrc $HOME/.vimrc
+mkdir -p $HOME/.vim/colors/
+ln -s $DOTFILES_PATH/vim/colors/molokai.vim $HOME/.vim/colors/molokai.vim
+ln -s $DOTFILES_PATH/vim/colors/wombat256mod.vim $HOME/.vim/colors/wombat256mod.vim
+ln -s $DOTFILES_PATH/vim/colors/wombat256.vim $HOME/.vim/colors/wombat256.vim
+ln -s $DOTFILES_PATH/vim/colors/wombat.vim $HOME/.vim/colors/wombat.vim
+ln -s $DOTFILES_PATH/gitconfig $HOME/.gitconfig
+
+VUNDLE_DIR="$HOME/.vim/bundle/Vundle.vim" 
+if [ ! -d $VUNDLE_DIR ]; then
+  echo "Installing Vundle..."
+  git clone https://github.com/gmarik/Vundle.vim.git $VUNDLE_DIR
+fi
+
+echo "Done!"
 
 # bashrc
